0cd6f1140de4c5013e1642a76d9e7318bb3a318b
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-24 23:08:48 +0530

    Update return type

--- src/basics/VariableTest.java
+++ src/basics/VariableTest.java
@@ -1,23 +1,32 @@
 package basics;
 
+import java.util.Calendar;
+
 public class VariableTest {
     // static int age = 28; // class level variable
     static String name = "Shailendra";
     char gender = 'M';
+    Calendar c;
     public static void main(String[] args) {
         int age = 28; // local variable
         System.out.println("My age is: " + age);
-        getMyName();
-        getMyAge();
+        String myName = getMyName();
+        // int myAge = getMyAge();
+        // System.out.println("my age: " + getMyAge());
+        System.out.println("My name is " + myName);
     }
 
-    public static void getMyAge() {
+    public static int getMyAge() {
         int age = 30;
-        System.out.println("My age from new function is: " + age);
+        // System.out.println("My age from new function is: " + age);
+        getMyName();
+        return age;
     }
-    public static void getMyName() {
-        System.out.println("My Name is " + name);
-        System.out.println("This is a second line");
+    public static String getMyName() {
+        // System.out.println("My Name is " + name);
+        // System.out.println("This is a second line");
+        name = "Srihith";
+        return name;
     }
     
 }
