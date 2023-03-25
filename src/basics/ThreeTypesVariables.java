6bac4e3a6225d06749b1e5702b79bd1669bb29eb
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-25 09:43:49 +0530

    Consists of 3 different variables within code

---
+++ src/basics/ThreeTypesVariables.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class ThreeTypesVariables {  
+    static int m=100;//static variable  
+    void method()  
+    {    
+        int n=90;//local variable    
+    }  
+    public static void main(String args[])  
+    {  
+        int data=50;//instance variable    
+    }  
+}//end of class    
+    
