8ddbd20e779e035a4b664dac7b2f84415afe57f5
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-25 10:28:44 +0530

    Variable update

---
+++ src/basics/LocalVariableExample.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class LocalVariableExample {
+    public static void main(String[] args){  
+         /*In the given code, a and b are both local variables of the main method, 
+    which means that they are only accessible within that method.
+Therefore, a and b are local variables of the main method, and they are of the integer type (int).
+The variable c is also a local variable of the main method and is of the integer type (int).
+So in summary, all variables in the given code are local variables of the main method and are of the integer type (int). */  
+        int a=10;    
+        int b=10;    
+        int c=a+b;    
+        System.out.println(c);    
+        }  
+    
+}
--- src/basics/VariableTest.java
+++ src/basics/VariableTest.java
@@ -7,17 +7,17 @@ public class VariableTest {
     static String name = "Shailendra";
     char gender = 'M';
     Calendar c;
-    public static void main(String[] args) {
-        int age = 28; // local variable
+    public static void main(String[] args) {     // public static are access modifiers, void is a written type, main is a method name, String[], args are parameters.
+        int age = 28;      // local variable
         System.out.println("My age is: " + age);
-        String myName = getMyName();
+        String myName = getMyName();      // invoking a method/function
         // int myAge = getMyAge();
         // System.out.println("my age: " + getMyAge());
         System.out.println("My name is " + myName);
     }
 
-    public static int getMyAge() {
-        int age = 30;
+    public static int getMyAge() {   //this is a static variable
+        int age = 30;     // local variable
         // System.out.println("My age from new function is: " + age);
         getMyName();
         return age;
