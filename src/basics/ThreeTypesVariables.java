0900cf3640ba1e8d85ec71eced1266057de5e985
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-25 09:53:15 +0530

    Comments on variables in detail.

--- src/basics/ThreeTypesVariables.java
+++ src/basics/ThreeTypesVariables.java
@@ -1,14 +1,19 @@
 package basics;
 
 public class ThreeTypesVariables {  
-    static int m=100;//static variable  
+     static int m=100; /*static variable - creates a single copy of the static variable, 
+     share it among all the instances of the class, 
+     Memory allocation for static variables happens only once when the class is loaded in the memory.*/
     void method()  
     {    
-        int n=90;//local variable    
+        int n=90;/*local variable - inside the body of the method, 
+        other methods in the class aren't even aware that the variable exists.*/
     }  
     public static void main(String args[])  
     {  
-        int data=50;//instance variable    
+        int data=50; //instance variable - variable declared inside the class but outside the body of the method.  
+        
+          
     }  
 }//end of class    
     
