5a943bd445f223628abeef616be13dacd70d5f60
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-27 16:50:35 +0530

    Example of default constructor with comments

---
+++ src/oops/DefaultConstructor.java
@@ -0,0 +1,13 @@
+package oops;
+//Java Program to create and call a default constructor  
+
+ class DefaultConstructor {
+     // creating a default constructor
+     DefaultConstructor(){System.out.println("Default constructor is created");}
+     //main method
+     public static void main(String[] args) {
+        //calling a default constructor
+        DefaultConstructor d = new DefaultConstructor();
+     }
+    
+}
