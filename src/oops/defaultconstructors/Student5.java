9cbea4e05298418d0234d6ae76814346b975dcde
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-27 18:04:23 +0530

    Overloading construction using three variable

---
+++ src/oops/defaultconstructors/Student5.java
@@ -0,0 +1,24 @@
+package oops.defaultconstructors;
+
+public class Student5 {
+    int id;
+    String name;
+    int age;
+
+    Student5(int i, String n){
+        id = i;
+        name = n;
+    }
+    Student5(int i,String n,int a){
+        id = i;
+        name = n;
+        age = a;
+    }
+void display(){System.out.println(age+" "+name+" "+id);}
+public static void main(String[] args) {
+    Student5 s1=new Student5(0, "kevin", 111);
+    Student5 s2=new Student5(24, "root", 222);
+    s1.display();
+    s2.display();
+}
+}
