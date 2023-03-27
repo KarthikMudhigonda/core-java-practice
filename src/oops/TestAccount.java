d94cbf708ccde1f55e20ed7db7b4d5e0a60d90aa
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-27 16:14:06 +0530

    Update on folder names

---
+++ src/oops/TestAccount.java
@@ -0,0 +1,39 @@
+package oops;
+
+ class Account {
+        int acc_no;
+        float amount;
+        String name;
+        void insert(int a, String n, float amt){
+            acc_no = a;
+            name = n;
+            amount = amt;
+        }
+    void deposit(float amt){
+        amount = amount+amt;
+        System.out.println(amt + " deposited");
+    }
+    void withdraw(float amt){
+        if(amount<amt){
+            System.out.println("insufficient funds");
+        }else {
+            amount = amount-amt;
+            System.out.println(amt + " withdrawn");
+        }
+    }
+void checkBalance(){System.out.println("Balance is: "+ amount);}
+void display(){System.out.println(acc_no+" "+name+" "+amount);}
+
+}
+class TestAccount{
+    public static void main(String[] args) {
+        Account a1 = new Account();
+        a1.insert(812345,"Ankit",1000);
+        a1.display();
+        a1.checkBalance();
+        a1.deposit(40000);
+        a1.checkBalance();
+        a1.withdraw(15000);
+        a1.checkBalance();
+    }
+}
