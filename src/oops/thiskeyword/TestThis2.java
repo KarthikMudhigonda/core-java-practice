e1d1c9eb059e522eeb9fab52bd100b2b24f340cf
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-28 20:10:50 +0530

    Issue with your development environment in vscode

---
+++ src/oops/thiskeyword/TestThis2.java
@@ -0,0 +1,15 @@
+package oops.thiskeyword;
+
+class A{  
+    
+A(){System.out.println("hello a");}  
+A(int x){  
+this();  
+System.out.println(x);  
+}  
+}  
+class TestThis2{  
+public static void main(String args[]){  
+A a = new A(10);  
+}
+}
