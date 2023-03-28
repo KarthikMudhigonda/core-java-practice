ef895e0376e5c82572abbbd57d2ebeb2f8fde188
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-28 11:16:53 +0530

    Using static method calculated cube

---
+++ src/oops/statickeyword/Calculate.java
@@ -0,0 +1,11 @@
+package oops.statickeyword;
+
+class Calculate{
+static int cube(int x){
+    return x*x*x;
+}
+public static void main(String[] args) {
+    int result = Calculate.cube(9);
+    System.out.println(result);
+}
+}
