2e13750257aa4816a413a795b7bae1ec29e6a851
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-28 10:30:52 +0530

    Program of the counter without static variable

---
+++ src/oops/statickeyword/Counter.java
@@ -0,0 +1,18 @@
+package oops.statickeyword;
+//Program of the counter without static variable
+//Java Program to demonstrate the use of an instance variable  
+//which get memory each time when we create an object of the class.  
+
+ class Counter {
+    int count = 0; // will get memory each time when the instance is created
+    Counter(){
+        count++;
+        System.out.println(count);
+    }
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+
+    }
+}
