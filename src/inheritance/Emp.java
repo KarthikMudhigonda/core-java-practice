8de4dfea570077242da6e5ae795e6ff0ad847bd7
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-29 07:38:05 +0530

    Meaningful example of aggregation

---
+++ src/inheritance/Emp.java
@@ -0,0 +1,28 @@
+package inheritance;
+
+public class Emp {
+    int id;
+    String name;
+    Address address;
+
+public Emp(int id, String name, Address address){
+    this.id = id;
+    this.name = name;
+    this.address = address;
+}
+void display(){
+    System.out.println(id+" "+name);
+    System.out.println(address.city+" "+address.state+" "+address.country);
+}
+    public static void main(String[] args) {
+        Address address1 =  new Address("Nzb", "TS", "India");
+        Address address2 = new Address("Hyd","TS", "India");
+
+        Emp e = new Emp(111,"Kevin",address1);
+        Emp e2 = new Emp(222,"Root",address2);
+
+        e.display();
+        e2.display();
+    }    
+
+}
