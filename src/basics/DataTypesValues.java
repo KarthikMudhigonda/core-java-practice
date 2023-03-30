c13b6d8456f66aae545202483d02ace7a0518665
KarthikMudhigonda <128830280+KarthikMudhigonda@users.noreply.github.com>
2023-03-30 08:44:11 +0530

    Eight primitive datatypes

---
+++ src/basics/DataTypesValues.java
@@ -0,0 +1,42 @@
+package basics;
+
+/*What is a data type?
+A data type is a classification of data which tells the compiler or interpreter
+how the programmer intends to use the data */
+
+public class DataTypesValues {
+    public static void main(String[] args) {
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.SIZE / 8 + " " + "Value in byte");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Short.SIZE);
+        System.out.println(Short.SIZE / 8 + " " + " Value in byte ");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.SIZE / 8 + " " + " Value in byte ");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.SIZE);
+        System.out.println(Long.SIZE / 8 + " " + " Value in byte ");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Float.SIZE);
+        System.out.println(Float.SIZE / 8 + " " + " Value in byte ");
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.SIZE);
+        System.out.println(Double.SIZE / 8 + " " + " Value in byte ");
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Character.SIZE);
+        System.out.println(Character.SIZE / 8 + " " + " Value in byte ");
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+        System.out.println(Boolean.TRUE);
+        System.out.println(Boolean.FALSE);
+
+    }
+
+}
